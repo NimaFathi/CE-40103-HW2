9767b296ecc223a23eae36db31600ef3a9dd33e7
Niwgoat <nimafathinimafathi@gmail.com>
2021-03-17 23:27:49 +0330

    Add methods and base structure of square class

---
+++ Area/src/Square.java
@@ -0,0 +1,15 @@
+public class Square extends Foursquare{
+    private float edge;
+    public Square(float edge) {
+        super(edge, edge);
+    }
+
+    @Override
+    public float computeArea() {
+
+    }
+
+    public float getEdge() {
+
+    }
+}
