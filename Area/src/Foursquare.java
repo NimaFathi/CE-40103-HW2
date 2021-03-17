e1329d598d7a13d9fad755bc845fa7aa353a7d87
Niwgoat <nimafathinimafathi@gmail.com>
2021-03-17 22:53:11 +0330

    Add rectangle

---
+++ Area/src/Foursquare.java
@@ -0,0 +1,14 @@
+public abstract class Foursquare {
+    private float width;
+    private float height;
+
+    public float getWidth() {
+        return this.width;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public abstract float computeArea();
+}
